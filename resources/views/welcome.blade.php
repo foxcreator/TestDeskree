aee6c4cae6ec09fd04db8cd2eb75fb30072d7221
foxcreator <foxcreatorg@gmail.com>
2023-10-10 15:44:58 +0300

    test image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,13 +7,13 @@
             <h6>Повернемо частину витрат з маркетплейсів, доставок продуктів, онлайн курсів, аптек</h6>
             <form action="/" class="col-md-6 mt-5">
                 <div class="input-group mb-2">
-                    <input type="text" class="form-control singup-input" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1">
+                    <input type="text" class="form-control singup-input" placeholder="Email" aria-label="Username" aria-describedby="basic-addon1">
                 </div>
                 <button class="btn btn-success w-100">Зареєструватися</button>
             </form>
         </div>
         <div class="col-md-5">
-            <img class="img-fluid" src="assets/img/logonosingin.png" alt="noimage =(">
+            <img class="img-fluid" src="{{ asset('assets/img/logonosingin.png') }}" alt="noimage =(">
         </div>
     </div>
 </div>
